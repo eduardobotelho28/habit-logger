96e269a68d6e6fb884bc74d46d20788ab54d5824
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2026-02-19 19:51:54 -0300

    habits and habit_logs migrations

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
    @if(auth()->check())
         <div class="max-w-4xl mx-auto px-6 pt-6 text-center">
             <p class="text-lg text-[#071013]/70">
-                Bem-vindo(a) a sua dashboard, <span class="font-semibold">{{ auth()->user()->name }}</span>!
+                Bem-vindo(a) à sua dashboard, <span class="font-semibold">{{ auth()->user()->name }}</span>!
             </p>
         </div>
     @endif
