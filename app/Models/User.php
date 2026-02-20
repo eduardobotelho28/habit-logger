f153b5860d3a2b9a3aa0300ea06cbe0a1f5f901f
Eduardo Botelho <126706543+eduardobotelho28@users.noreply.github.com>
2026-02-20 15:40:31 -0300

    add relationships model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -43,4 +44,17 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    //um usuário pode ter muitos hábitos.
+    public function habits () : HasMany 
+    {
+        return $this->hasMany(Habit::class);
+    }
+
+    //um usuário pode ter muitos registros de hábitos.
+    public function habitLogs () : HasMany 
+    {
+        return $this->hasMany(HabitLog::class);
+    }
+
 }
